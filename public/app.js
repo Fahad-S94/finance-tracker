5aa6272626a635d76c315f3cabbb0419766b4970
Fahad Shoaib <f.shoaib1580@gmail.com>
2024-02-20 01:26:48 -0500

    typescript types and DOM manipulation

--- public/app.js
+++ public/app.js
@@ -1,12 +1,11 @@
+"use strict";
 // let result: Function;
 // let sayHello: (a: string, b:string) => void;
 // sayHello = (name: string, greet: string) => {
 //     console.log(`Hello ${name}, ${greet}`);
 // }
-
 // example 2
 // let calc: (a: number, b: number, c: string) => number;
-
 // calc = (numOne: number, numTwo: number, action: string) => {
 //   if (action === 'add') {
 //     return numOne + numTwo;
@@ -14,37 +13,26 @@
 //     return numOne - numTwo;
 //   }
 // }
-
 // example 3
 // let logDetails: (obj: {name: string, age: number}) => void;
 // type person = {name: string, age: number}
 // logDetails = (ninja: person) => {
 //   console.log(`${ninja.name} is ${ninja.age} years old`);
 // }
-
 // const anchor = document.querySelector('a')!;
 // if(anchor) {
 //   console.log(anchor.href);
 // }
 // console.log(anchor.href);
-
 //const form = document.querySelector('form')!;
 // const form = document.querySelector('.new-item-form') as HTMLFormElement;
 // console.log(form.children);
-
 // inputs
-const type = document.querySelector('#type') as HTMLInputElement;
-const toFrom = document.querySelector('#toFrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
-
-form.addEventListener('submit', (e: Event) => {
-  e.preventDefault();
-
-  console.log(
-    type.value, 
-    toFrom.value, 
-    details.value, 
-    amount.valueAsNumber
-  );
+const type = document.querySelector('#type');
+const toFrom = document.querySelector('#toFrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
 });
